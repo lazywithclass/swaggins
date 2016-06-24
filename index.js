0af0366477cb9d42cf027293f0565a20880e699b
lazywithclass <montecristoh@gmail.com>
2016-06-24 18:30:31 +0100

    making request.body available

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ exports.probe = () => {
   http.request = function(options, callback) {
     function interceptor() {
       var res = arguments[0].req.res;
+      // TODO this is terrible, isn't the body somewhere
+      // in the req or res?
+      res.req.body = options.body
       swagger.write(res);
 
       if (callback) callback();
