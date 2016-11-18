6c2127adc0b5ec0b039e859832c5809ed258a99a
lazywithclass <montecristoh@gmail.com>
2016-11-18 17:46:18 +0000

    added a more complete example

--- examples/express/server.js
+++ examples/express/server.js
@@ -1,11 +1,51 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    jsonparser = require('body-parser').json({ type: 'application/*'});
+
+app.use(jsonparser);
 
 app.get('/answer/:about', function (req, res) {
   if (req.params.about === 'everything') res.send('42');
   else res.send(404);
 });
 
+app.post('/answer/math', function(req, res) {
+  var solvedExpressions = req.body.expressions.map(function(expression) {
+    return eval(expression);
+  });
+  res.json(solvedExpressions).status(201);
+});
+
+
+var db = {};
+
+app.get('/db', function(req, res) {
+  res.status(200).json({ items: db });
+});
+
+app.post('/db', function(req, res) {
+  db[req.body.item.key] = req.body.item.value;
+  res.status(201).json({ newitem: req.body.item });
+});
+
+app.get('/db/:id', function(req, res) {
+  res.status(200).json({
+    item: { key: parseInt(req.params.id, 10), value: db[req.params.id] }
+  });
+});
+
+app.put('/db/:id', function(req, res) {
+  db[req.params.id] = req.body.newvalue;
+  res.status(200).json({
+    item: { key: parseInt(req.params.id, 10), value: db[req.params.id] }
+  });
+});
+
+app.delete('/db/:id', function(req, res) {
+  delete db[req.params.id];
+  res.status(200).json({});
+});
+
 app.listen(1337, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 1337!');
 });
--- examples/express/test-with-probe.js
+++ examples/express/test-with-probe.js
@@ -5,14 +5,133 @@ require('../../index').probe();
 
 describe('server', function() {
 
-  it('answers to /answer with 42', function(done) {
-    request(
-      'http://localhost:1337/answer/everything',
-      function cb(err, res, body) {
+  describe('answer', function() {
+
+    it('answers to /answer with 42', function(done) {
+      var options = {
+        headers: {
+          'x-swaggins-tag': 'answer',
+          'x-swaggins-path': '/answer/{about}',
+          'x-swaggins-endpoint-description': 'The answer to life the universe and everything'
+        },
+        uri: 'http://localhost:1337/answer/everything'
+      };
+      request(options, function(err, res, body) {
         expect(body).to.equal('42');
         done();
-      }
-    );
+      });
+    });
+
+    it('accepts math expressions returning the result', function(done) {
+      var options = {
+        headers: {
+          'x-swaggins-tag': 'answer',
+          'x-swaggins-endpoint-description': 'Evaluate your math expressions'
+        },
+        uri: 'http://localhost:1337/answer/math',
+        method: 'POST',
+        json: {
+          expressions: [
+            '1 + 1 + 1',
+            '41 + 1'
+          ]
+        }
+      };
+      request(options, function(err, res, body) {
+        expect(body).to.deep.equal([ 3, 42 ]);
+        done();
+      });
+    });
+
+  });
+
+  describe('db', function() {
+
+    it('adds an item', function(done) {
+      var options = {
+        headers: {
+          'x-swaggins-tag': 'db',
+          'x-swaggins-path': '/db/{id}',
+          'x-swaggins-endpoint-description': 'Add an item to the database'
+        },
+        uri: 'http://localhost:1337/db',
+        method: 'POST',
+        json: { item: { key: 1, value: 2 } }
+      };
+      request(options, function(err, res, body) {
+        expect(res.statusCode).to.equal(201);
+        expect(body).to.deep.equal({ newitem: { key: 1, value: 2 }});
+        done();
+      });
+    });
+
+    it('gets an item', function(done) {
+      var options = {
+        headers: {
+          'x-swaggins-tag': 'db',
+          'x-swaggins-path': '/db/{id}',
+          'x-swaggins-endpoint-description': 'Get an item from the database'
+        },
+        uri: 'http://localhost:1337/db/1'
+      };
+      request(options, function(err, res, body) {
+        expect(res.statusCode).to.equal(200);
+        expect(JSON.parse(body)).to.deep.equal({
+          item: { key: 1, value: 2 }
+        });
+        done();
+      });
+    });
+
+    it('updates an item', function(done) {
+      var options = {
+        headers: {
+          'x-swaggins-tag': 'db',
+          'x-swaggins-path': '/db/{id}',
+          'x-swaggins-endpoint-description': 'Update an item in the database'
+        },
+        uri: 'http://localhost:1337/db/1',
+        method: 'PUT',
+        json: { newvalue: 3 }
+      };
+      request(options, function(err, res, body) {
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.deep.equal({ item: { key: 1, value: 3 }});
+        done();
+      });
+    });
+
+    it('deletes an item', function(done) {
+      var options = {
+        headers: {
+          'x-swaggins-tag': 'db',
+          'x-swaggins-path': '/db/{id}',
+          'x-swaggins-endpoint-description': 'Delete an item from the database'
+        },
+        uri: 'http://localhost:1337/db/1',
+        method: 'DELETE'
+      };
+      request.del(options, function(err, res, body) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('gets all db', function(done) {
+      var options = {
+        headers: {
+          'x-swaggins-tag': 'db',
+          'x-swaggins-path': '/db',
+          'x-swaggins-endpoint-description': 'Return the whole database'
+        },
+        uri: 'http://localhost:1337/db'
+      };
+      request(options, function(err, res, body) {
+        expect(res.statusCode).to.equal(200);
+        expect(JSON.parse(body)).to.deep.equal({ items: {}});
+        done();
+      });
+    });
 
   });
 
